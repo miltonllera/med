110bb8865d4d1a7218c787c95f83116af7f4d3c1
Milton Montero <lleramontero@gmail.com>
2023-11-13 16:23:21 +0100

    Added LGA strategy.

--- scripts/ncas_salamander_emoji.sh
+++ scripts/ncas_salamander_emoji.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Testing evolutionary training of NCAs to generate a salamander emoji (as in Mordvintsev et al.)
+
 # standard nca
 python -m bin.train experiment=single_emoji_nca_evo trainer.strategy.popsize=200
 
-# squashing outputs
-python -m bin.train experiment=single_emoji_nca_evo trainer.strategy.popsize=200 model=nca_bounded_output
+# standard nca but fitted using LGA (Lange et al., 2023)
+python -m bin.train experiment=single_emoji_nca_evo trainer.strategy.popsize=200 strategy@trainer.strategy=lga
 
-# squashing updates
-python -m bin.train experiment=single_emoji_nca_evo trainer.strategy.popsize=200 model=nca_bounded_updates
+# # squashing outputs
+# python -m bin.train experiment=single_emoji_nca_evo trainer.strategy.popsize=200 model=nca_bounded_output
 
-# squashing both
-python -m bin.train experiment=single_emoji_nca_evo trainer.strategy.popsize=200 model=nca_all_bounded
+# # squashing updates
+# python -m bin.train experiment=single_emoji_nca_evo trainer.strategy.popsize=200 model=nca_bounded_updates
+
+# # squashing both
+# python -m bin.train experiment=single_emoji_nca_evo trainer.strategy.popsize=200 model=nca_all_bounded
