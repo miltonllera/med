0c3078ac8012c9207d99f6a977342dcc463b2952
Milton Montero <lleramontero@gmail.com>
2023-12-15 15:49:38 +0100

    Run dummy ES experiment.

--- scripts/zelda_nca_dna.sh
+++ scripts/zelda_nca_dna.sh
@@ -12,14 +12,14 @@
 #   +model@model.dna=dna_list model.dna.n_dnas=\${task.popsize}
 
 # nca + dna sampler (using CMA emitter in the inner loop)
-CUDA_VISIBLE_DEVICES="2" python -m bin.train \
-  experiment=qd_dna_zelda_gen \
-  qd@task.qd_algorithm=cmame  \
-  +task.qd_algorithm.emitter.genotype_dim=32 \
-  +model@model.dna=dna_iid_sampler \
+# CUDA_VISIBLE_DEVICES="2" python -m bin.train \
+#   experiment=qd_dna_zelda_gen \
+#   qd@task.qd_algorithm=cmame  \
+#   +task.qd_algorithm.emitter.genotype_dim=32 \
+#   +model@model.dna=dna_iid_sampler \
 
 # # dummy strategy
-# CUDA_VISIBLE_DEVICES="1" python -m bin.train experiment=dummy_qd_dna_zelda_gen \
-#   qd@task.qd_algorithm=cmame \
-#   +task.qd_algorithm.emitter.genotype_dim=32 \
-#   +model@model.dna=dna_iid_sampler
+CUDA_VISIBLE_DEVICES="3" python -m bin.train experiment=dummy_qd_dna_zelda_gen \
+  qd@task.qd_algorithm=cmame \
+  +task.qd_algorithm.emitter.genotype_dim=32 \
+  +model@model.dna=dna_iid_sampler
