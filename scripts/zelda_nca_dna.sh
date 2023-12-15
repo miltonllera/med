0baff9ed4f887038038d707730714e75a4a2ffc6
Milton Montero <lleramontero@gmail.com>
2023-12-15 15:35:26 +0100

    Refactor configs. Run with CMA-ME.

--- scripts/zelda_nca_dna.sh
+++ scripts/zelda_nca_dna.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # nca + dna sampler
-# CUDA_VISIBLE_DEVICES="1" python -m bin.train experiment=qd_dna_zelda_gen \
+# CUDA_VISIBLE_DEVICES="1" python -m bin.train \
+#   experiment=qd_dna_zelda_gen \
+#   model=nca_dna_all_alive \
 #   +model@model.dna=dna_iid_sampler
 
 # # nca + dna list
@@ -10,13 +12,14 @@
 #   +model@model.dna=dna_list model.dna.n_dnas=\${task.popsize}
 
 # nca + dna sampler (using CMA emitter in the inner loop)
-CUDA_VISIBLE_DEVICES="1" python -m bin.train experiment=qd_dna_zelda_gen \
-  qd@task.qd_algorithm=cmame \
+CUDA_VISIBLE_DEVICES="2" python -m bin.train \
+  experiment=qd_dna_zelda_gen \
+  qd@task.qd_algorithm=cmame  \
   +task.qd_algorithm.emitter.genotype_dim=32 \
-  +model@model.dna=dna_iid_sampler
+  +model@model.dna=dna_iid_sampler \
 
-# dummy strategy
-CUDA_VISIBLE_DEVICES="1" python -m bin.train experiment=qd_dna_zelda_gen \
-  qd@task.qd_algorithm=cmame \
-  +task.qd_algorithm.emitter.genotype_dim=32 \
-  +model@model.dna=dna_iid_sampler
+# # dummy strategy
+# CUDA_VISIBLE_DEVICES="1" python -m bin.train experiment=dummy_qd_dna_zelda_gen \
+#   qd@task.qd_algorithm=cmame \
+#   +task.qd_algorithm.emitter.genotype_dim=32 \
+#   +model@model.dna=dna_iid_sampler
